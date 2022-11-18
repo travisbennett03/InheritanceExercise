6d3c4b0632419b3443a644243f6c147d9ab9567d
travisbennett03 <tbennett1996@yahoo.com>
2022-11-18 10:37:56 -0600

    initial commit

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -29,11 +29,39 @@ namespace Inheritance
              * Creatively display the class member values 
              */
 
+            var myBird = new Bird();
+            myBird.WingColor = "blue";
+            myBird.CanFly = true;
+            myBird.DoMigrate = true;
+            myBird.BeakLength = 3.5;
+
+
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
              *  
              * Creatively display the class member values 
              */
+
+            var lizard = new Reptile()
+            {
+                IsColdBlooded = true,
+                IsScaley = true,
+                Habitat = "swamp",
+                CanGrowTail = true,
+            };
+            
+            var myAnimals = new Animal[] {myBird, lizard};
+
+            foreach(var animal in myAnimals)
+            {
+                Console.WriteLine($"Alive:{animal.IsAlive}");
+                Console.WriteLine($"Age: {animal.Age} years old");
+                Console.WriteLine($"It has: {animal.LegCount} legs");
+                Console.WriteLine($"It lives by {animal.LandSearAir}");
+            }
+            
+
+            
         }
     }
 }
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Reptile : Animal
+    {
+        public Reptile()
+        {
+            IsAlive = true;
+            LegCount = 4;
+            LandSearAir = "Land";
+            Age = 0;
+        }
+
+        public bool IsColdBlooded { get; set; }
+        public bool IsScaley { get; set; }
+        public string Habitat { get; set; }
+        public bool CanGrowTail { get; set; }
+    }
+}
